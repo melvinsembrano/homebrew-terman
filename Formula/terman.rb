ee56ba431f28772176d71e872b046ff6cd6fcb79
Melvin Sembrano <melv@hey.com>
2026-07-21 10:06:13 +1000

    chore: update terman to v0.7.1

--- Formula/terman.rb
+++ Formula/terman.rb
@@ -1,28 +1,28 @@
 class Terman < Formula
   desc "Terminal API Client: TUI and CLI for building and running HTTP requests"
   homepage "https://github.com/melvinsembrano/terman"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/melvinsembrano/terman/releases/download/v#{version}/terman-#{version}-darwin-amd64.tar.gz"
-      sha256 "45e75d868f1001b75e35c2ec7803327546faf6b01dd239b5a624e2b1886b97e2"
+      sha256 "8361ba2054b9270882e6627ebbd1ef712c88252020264baf09a6d41d8370bf0b"
     end
     on_arm do
       url "https://github.com/melvinsembrano/terman/releases/download/v#{version}/terman-#{version}-darwin-arm64.tar.gz"
-      sha256 "afa87759a98a13daf59e61e9178d6bec48354e4f198c9c17951e064d27c4bd0d"
+      sha256 "24b49d687ef5e0db0cf06db1631c24defe34593fb3777d020bf99262c512ff5e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/melvinsembrano/terman/releases/download/v#{version}/terman-#{version}-linux-amd64.tar.gz"
-      sha256 "cab9bc693534e7badaac47c0013567d2030bc5dbdbfa0eb3f88f85bef54685fa"
+      sha256 "9e1c597262986d65719c7093b4458263c31434d940ce33d5d25d38f32b2ac8ea"
     end
     on_arm do
       url "https://github.com/melvinsembrano/terman/releases/download/v#{version}/terman-#{version}-linux-arm64.tar.gz"
-      sha256 "978f1a868696825669d1f110a6fe9dac993f881395b36191115df4c996d8e42e"
+      sha256 "d0d8732c9f75e51fd292d214279d2bf0b25b3d4506176bc618c4f4f6eb35aa27"
     end
   end
 
